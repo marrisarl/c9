332a3f4ca4703f6b198ec32fbe0612f6c3160961
whale <42706990+marrisarl@users.noreply.github.com>
2023-11-17 16:19:36 +0700

    Update install.php

--- install.php
+++ install.php
@@ -10,3 +10,6 @@ https://www.mediafire.com/file/579bh8pecjs85mq/IDM.6.38.b.02.kuyhAa.Me+(1).zip/f
 
 4k downloader
 https://www.mediafire.com/file/0obbvx5eoy7iluh/4K_Video_Downloader_4.24.0.5340_2023_Tested_by_Bicfic.com-20230525T130339Z-001.zip/file
+
+wallpaper
+https://www.mediafire.com/file/10znwhi43rxwstr/SHIRO+RDP.png/file
