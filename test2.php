34e37c4cee28a47d6445a4775cdb52d6ca613ec4
whale <42706990+marrisarl@users.noreply.github.com>
2023-11-17 17:05:18 +0700

    Update test2.php

--- test2.php
+++ test2.php
@@ -2,3 +2,6 @@ https://www.mediafire.com/file/iqu5bei2vf5tk2j/rdpwrap.txt/file
 
 
 https://github.com/stascorp/rdpwrap/releases
+
+
+https://github.com/asmtron/rdpwrap/raw/master/autoupdate_v1.1.zip
