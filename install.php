6df4bb36483eb3992388901055b782b19577a5bc
whale <42706990+marrisarl@users.noreply.github.com>
2023-11-17 16:10:00 +0700

    Create install.php

---
+++ install.php
@@ -0,0 +1,12 @@
+
+port 7474
+https://www.mediafire.com/file/y7o46nu4utsimne/74-74.reg/file
+
+sable update
+https://www.mediafire.com/file/tf6y67qhxl2ktfe/WinUpdBlockerv12+(2).zip/file
+
+idm
+https://www.mediafire.com/file/579bh8pecjs85mq/IDM.6.38.b.02.kuyhAa.Me+(1).zip/file
+
+4k downloader
+https://www.mediafire.com/file/0obbvx5eoy7iluh/4K_Video_Downloader_4.24.0.5340_2023_Tested_by_Bicfic.com-20230525T130339Z-001.zip/file
