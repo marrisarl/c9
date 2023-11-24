1a1030f24c8390f109b73fa763fa417ba48f2d4d
whale <42706990+marrisarl@users.noreply.github.com>
2023-11-24 14:32:20 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Contoh Gambar di HTML</title>
-</head>
-<body>
-  <h1>Menampilkan Gambar di HTML</h1>
-  <p>Berikut ini adalah gambar sawah:</p>
-  <p>
-    <img src="https://www.petanikode.com/img/html/img/contoh-gambar-html.webp" />
-  </p>
-</body>
-</html>
+https://www.mediafire.com/file/3rutjbrqtdw3pry/SHIRO-RDP-3.png/file
